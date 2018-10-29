b2301c320d0a2a48aa3472a7a206bb4242ac3fe3
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 18:17:48 -0500

    made a simple form on profile.html

--- create.php
+++ create.php
@@ -4,12 +4,12 @@ $weight = $_POST['weight'];
 $age = $_POST['age'];
 
 //connect
-$conn = new mysqli('localhost', 'teamsaauuwwce_teamsauce', 'Teamsauce', 'teamsaauuwwce_tempdatabase');
+$conn = new mysqli('127.0.0.1', 'teamsaauuwwce_teamsauce', 'Teamsauce', 'teamsaauuwwce_tempdatabase');
     if(mysqli_connect_errno()) {
         exit('Connect failed: '. mysqli_connect_error());
 
 //generate the query (doesn't add id because id is autoincremented)
-$query = "INSERT INTO Users (" + id + ", " + height + ", " + weight + ", " + age + ");";
+$query = "insert into Users (" + id + ", " + height + ", " + weight + ", " + age + ");";
 //insert and close.
 mysqli_query($conn, $query);
 mysqli_close($conn);
