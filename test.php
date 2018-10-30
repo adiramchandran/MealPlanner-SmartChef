1a1dc3ba181d66bac1bd5be31484b60557926473
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 20:57:40 -0500

    made a simple form on profile.html

--- test.php
+++ test.php
@@ -7,43 +7,57 @@
 <?php
 session_start();
 $_SESSION['message'] = '';
-$mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-$link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
+$mysqli = new mysqli("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
+
+$link = mysqli_connect("127.0.0.1", "thullupolls_root", "Surabhiharish", "thullupolls_thullupolls");
 
 if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
 
-echo "Success: A proper connection top MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-mysqli_close($link);
 
+mysqli_close($link);
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-      $height = $mysqli->real_escape_string($_POST['height']);
-      $weight = $mysqli->real_escape_string($_POST['weight']);
-      $age = $mysqli->real_escape_string($_POST['age']);
-      $sql = "INSERT INTO User (height, weight, age) " . "VALUES ('$height', '$weight', '$age')";
-      if(($mysqli->query($sql) === true)){
-          header("Inserted");
-          exit();
-      }
-      else{
-          $_SESSION['message'] = "Account was not created:(";
-      }
+    if($_POST['password'] == $_POST['confirmpassword']){
+        $name = $mysqli->real_escape_string($_POST['name']);
+        $id = $mysqli->real_escape_string($_POST['id']);
+        $password = $mysqli->real_escape_string($_POST['password']);
+        $_SESSION['name'] = $name;
+        $_SESSION['id'] = $id;
+        $sql = "INSERT INTO User (id, name, password) " . "VALUES ('$id', '$name', '$password')";
+
+        if(($mysqli->query($sql) === true)){
+						header("Location: http://thullupolls.web.illinois.edu/signin.php");
+						exit();
+        }
+        else{
+            $_SESSION['message'] = "Account was not created:(";
+        }
+
+    }
+    else{
+        $_SESSION['message'] = "Two passwords do not match! Please type a valid password.";
+    }
 }
 $mysqli->close();
 ?>
 
 <html>
 	<head>
+	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Create Profile</title>
-
-
+	<title>Thullu Polls &mdash; Share Your Opinions</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
 	</head>
+
 	<body>
     <form method="post" action="profile.php" id="myForm" enctype="multipart/form-data" name="myForm" >
       <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
@@ -53,6 +67,5 @@ $mysqli->close();
       <input type="submit" name="submit"/>
     </div>
     </form>
-
 	</body>
 </html>
