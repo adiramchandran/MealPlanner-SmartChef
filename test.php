d5eca00f6f23d6538887c4febc8d9dac890bc14c
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 20:49:22 -0500

    made a simple form on profile.html

--- test.php
+++ test.php
@@ -8,19 +8,6 @@
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-$link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
 // if($_SERVER['REQUEST_METHOD'] == 'POST'){
 //     if($_POST['password'] == $_POST['confirmpassword']){
 //         $name = $mysqli->real_escape_string($_POST['name']);
@@ -91,99 +78,14 @@ $mysqli->close();
 
 	</head>
 	<body>
-
-	<div class="gtco-loader"></div>
-
-	<div id="page">
-
-		<nav class="gtco-nav" role="navigation">
-			<div class="container">
-
-				<div class="row">
-					<div class="col-xs-10 text-right fh5co-top-social">
-						<ul class="gtco-social">
-							<li><a href="#" class="icon-twitter"></i></a></li>
-							<li><a href="#" class="icon-dribbble"></i></a></li>
-							<li><a href="#" class="icon-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		</nav>
-		<div id="gtco-intro">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-md-offset-1 text-center">
-						<div class="dt js-height">
-							<div class="dtc animate-box">
-								<a class="topnav" href="index.html" title="Homepage">Home</a>
-								<h2 class="gradient-text">Create an account</h2>
-								<form class="form" action="signup.php" method="post" enctype="multipart/form-data" autocomplete="off">
-						      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-						      <input type="text" placeholder="Full Name" name="name" required />
-						      <input type="text" placeholder="User Name" name="id" required />
-						      <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
-						      <input type="password" placeholder="Confirm Password" name="confirmpassword" autocomplete="new-password" required />
-						      <input type="submit" value="verify" name="Create Account" class="btn btn-block btn-primary" />
-
-						      <div class="module">
-						    </form>
-								<!-- <p><a href="#" class="btn btn-gradient">Sign In</a></p>
-								<p><a href="#" class="btn btn-gradient">Sign up</a></p> -->
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-
-
-		<footer id="fh5co-footer">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
-						<div class="fh5co-footer-widget">
-							<a href="index.html">Motion <sup>&trade;</sup></a> Free HTML5 &copy; All Rights Reserved.  <br> Designed by <a href="http://gettemplates.co" target="_blank">GetTemplates.co</a> Images: <a href="http://pixeden.com" target="_blank">Pixeden</a>
-						</div>
-					</div>
-					<div class="col-md-3 col-md-push-1">
-						<div class="fh5co-footer-widget">
-							<p><a href="tel://+1 234 567 8910">+1 234 567 8910</a> <br> <a href="#">info@yourdomain.com</a></p>
-						</div>
-					</div>
-					<div class="col-md-4 col-md-push-1">
-						<div class="fh5co-footer-widget gtco-social-wrap">
-							<ul class="gtco-social">
-								<li><a href="#" class="icon-twitter"></a></li>
-								<li><a href="#" class="icon-dribbble"></a></li>
-								<li><a href="#" class="icon-instagram"></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-
-
-	</div>
-
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
-	</div>
-
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-
-	<!-- Main -->
-	<script src="js/main.js"></script>
+    <form method="post" action="profile.php" id="myForm" enctype="multipart/form-data" name="myForm" >
+      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
+      <label for="height">Height:</label>  <input type="text" name="height" /><br />
+      <label for="weight">Weight:</label> <input type="text" name="weight"><br />
+      <label for="age">Age:</label> <input type="text" name="age"><br />
+      <input type="submit" name="submit"/>
+    </div>
+    </form>
 
 	</body>
 </html>
