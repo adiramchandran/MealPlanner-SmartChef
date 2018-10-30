65bbf7678f4335ee0e3426ca76d0371989d6a67e
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 21:58:45 -0500

    made a simple form on profile.html

--- test.php
+++ test.php
@@ -9,9 +9,9 @@ session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
 $link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-// $height = $_POST['height']);
-// $weight = $_POST['weight']);
-// $age = $_POST['age']);
+$height = (int)$_POST['height']);
+$weight = (int)$_POST['weight']);
+$age = (int)$_POST['age']);
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -25,16 +25,16 @@ echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($link);
 
-// if($_SERVER['REQUEST_METHOD'] == 'POST') {
-//   $sql = "INSERT INTO User (height, weight, age) " . "VALUES ('$number', '$numbe', '$number')";
-//
-//   if(($mysqli->query($sql) === true) {
-//   		header("Inserted!");
-//   }
-//   else {
-//       $_SESSION['message'] = "Account was not created:(";
-//   }
-// }
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $sql = "INSERT INTO User (height, weight, age) " . "VALUES ('$height', '$weight', '$age')";
+
+  if(($mysqli->query($sql) === true) {
+  		header("Inserted!");
+  }
+  else {
+      $_SESSION['message'] = "Account was not created:(";
+  }
+}
 
 $mysqli->close();
 ?>
