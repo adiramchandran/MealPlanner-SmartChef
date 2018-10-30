035d775457a3534220bcedecbba9e16c98489f86
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 20:28:53 -0500

    made a simple form on profile.html

--- profile.php
+++ profile.php
@@ -17,33 +17,33 @@ session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
 $link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
 //
+// if (!$link) {
+//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
+// }
+
 // echo "Success: A proper connection top MySQL was made! The my_db database is great." . PHP_EOL;
 // echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-//
-// mysqli_close($link);
-// if($_SERVER['REQUEST_METHOD'] == 'POST'){
-//         $height = $_POST['height'];
-//         $weight = $_POST['weight'];
-//         $age = $_POST['age'];
-//         $sql = "INSERT INTO Users (height, weight, age) " . "VALUES ('$height', '$weight', '$age')";
-//
-//         if(($mysqli->query($sql) === true)){
-//          $_SESSION['message'] = "User was created";
-//         }
-//         else{
-//             $_SESSION['message'] = "Account was not created:(";
-//         }
-//
-//     }
-// }
+
+mysqli_close($link);
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $height = $_POST['height'];
+        $weight = $_POST['weight'];
+        $age = $_POST['age'];
+        $sql = "INSERT INTO Users (height, weight, age) " . "VALUES ('$height', '$weight', '$age')";
+
+        if(($mysqli->query($sql) === true)){
+         $_SESSION['message'] = "User was created";
+        }
+        else{
+            $_SESSION['message'] = "Account was not created:(";
+        }
+
+    }
+}
 $mysqli->close();
 ?>
 <html>
