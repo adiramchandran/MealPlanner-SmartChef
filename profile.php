c6d3d6362217922dc923f46df5d35287677da4d4
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 22:21:20 -0500

    made a simple form on profile.html

--- profile.php
+++ profile.php
@@ -1,56 +1,63 @@
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
-<title></title>
-<link rel="stylesheet" href="w3.css">
+	<title></title>
+	<link rel="stylesheet" href="w3.css">
 </head>
 
 <?php
 session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-// if (!$mysqli) {
-//     echo "Error";
-// }
+$link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
 
-// $link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-//
-// if (!$link) {
-//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
-// echo "Success: A proper connection top MySQL was made! The my_db database is great." . PHP_EOL;
-// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-//
-// mysqli_close($link);
-// if($_SERVER['REQUEST_METHOD'] == 'POST'){
-//         $height = $_POST['height'];
-//         $weight = $_POST['weight'];
-//         $age = $_POST['age'];
-//         $sql = "INSERT INTO Users (height, weight, age) " . "VALUES ('$height', '$weight', '$age')";
-//
-//         if(($mysqli->query($sql) === true)){
-//          $_SESSION['message'] = "User was created";
-//         }
-//         else{
-//             $_SESSION['message'] = "Account was not created:(";
-//         }
-// }
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $height = $_POST['height'];
+  $weight = $_POST['weight'];
+  $age = $_POST['age'];
+  $sql = "INSERT INTO Users (height, weight, age) " . " VALUES ($height, $weight, $age)";
+
+  if(mysqli_query($mysqli, $sql) === true) {
+  		header("Inserted!");
+  }
+  else {
+      $_SESSION['message'] = "Account was not created:(";
+  }
+}
+
 $mysqli->close();
 ?>
+
 <html>
-<head>
-<head>
-<body>
-  <form method="post" action="profile.php" id="myForm" enctype="multipart/form-data" name="myForm" >
-    <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-    <label for="height">Height:</label>  <input type="text" name="height" /><br />
-    <label for="weight">Weight:</label> <input type="text" name="weight"><br />
-    <label for="age">Age:</label> <input type="text" name="age"><br />
-    <input type="submit" name="submit"/>
-  </div>
-  </form>
-</body>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Create Profile</title>
+	</head>
+
+	<body>
+
+    <form class="form" action="profile.php" method="post" enctype="multipart/form-data" autocomplete="off">
+      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
+      <input type="text" placeholder="Height" name="height" required />
+      <input type="text" placeholder="Weight" name="weight" required />
+      <input type="text" placeholder="Age" name="age" required />
+      <input type="submit" value="Submit" name="Create Account" class="btn btn-block btn-primary" />
+
+      <div class="module">
+    </form>
+
+
+	</body>
 </html>
