eade612cf0da31534e9c13f9abdbd1add1dc46fb
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-29 20:03:43 -0500

    made a simple form on profile.html

--- create.php
+++ create.php
@@ -8,6 +8,20 @@ $conn = new mysqli('127.0.0.1', 'teamsaauuwwce_teamsauce', 'Teamsauce', 'teamsaa
     if(mysqli_connect_errno()) {
         exit('Connect failed: '. mysqli_connect_error());
 
+$link = mysqli_connect("127.0.0.1", 'teamsaauuwwce_teamsauce', 'Teamsauce', 'teamsaauuwwce_tempdatabase');
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+if (!conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
 //generate the query (doesn't add id because id is autoincremented)
 $query = "INSERT INTO Users (height, weight, age) VALUES ('".$height."', '".$weight."', '".$age."')";
 
