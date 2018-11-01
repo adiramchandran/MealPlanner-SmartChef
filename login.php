477a5595da83d8ccac3693e6461f0b9a54c42786
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-31 19:42:58 -0500

    made a simple form on profile.html

--- login.php
+++ login.php
@@ -3,6 +3,19 @@ session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
 
+$link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   if($_POST['password'] == $_POST['confirm-password']){
     $email = $mysqli->real_escape_string($_POST['email']);
@@ -15,7 +28,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
       echo "New record created successfully. Your ID is: " . $last_id;
     }
     else {
-        $_SESSION['message'] = "Account was not created:(";
+      echo "Account could not be created";
+      $_SESSION['message'] = "Account was not created:(";
     }
   }
   else {
