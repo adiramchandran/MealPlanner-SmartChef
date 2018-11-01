5f52a75b61a4725e9d53376881d590b9c490b452
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-31 19:59:03 -0500

    made a simple form on profile.html

--- login.php
+++ login.php
@@ -160,7 +160,7 @@ $mysqli->close();
         <button>create</button>
         <p class="message">Already registered? <a href="#">Sign In</a></p>
       </form>
-      <form class="login-form form" action="login.php" method="post" enctype="multipart/form-data">
+      <form class="login-form" action="login.php" method="post" enctype="multipart/form-data">
         <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
         <input type="text" placeholder="email address" name="email" required/>
         <input type="text" placeholder="username" name="username" required/>
