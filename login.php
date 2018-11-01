b8208b9bd39070bb410925edb87e87d2b7bd6122
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-31 20:03:59 -0500

    made a simple form on profile.html

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if(mysqli_query($mysqli, $sql) === true) {
       echo "Account created successfully!";
-      header("Location: index.php");
+      header("Location: index.html");
       exit();
     }
     else {
