ab46305e04223913286029cbedfc2d29d4c37a05
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-31 20:07:39 -0500

    made a simple form on profile.html

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if(mysqli_query($mysqli, $sql) === true) {
       echo "Account created successfully!";
-      header("Location: index.html");
+      header("Location: http://www.teamsaauuwwce.web.illinois.edu");
       exit;
     }
     else {
