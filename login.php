7cd0f26bb1a1d31494f551e96c4d9b0321b42060
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-10-31 19:19:48 -0500

    made a simple form on profile.html

--- login.php
+++ login.php
@@ -1,165 +1,24 @@
-<?php
-session_start();
-$_SESSION['message'] = '';
-$mysqli = new mysqli("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-// $link = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-//
-// if (!$link) {
-//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-//     exit;
-// }
-//
-// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-
-mysqli_close($link);
-
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $height = $_POST['height'];
-  $weight = $_POST['weight'];
-  $age = $_POST['age'];
-  $sql = "INSERT INTO User (ID, Height, Weight, Age) " . " VALUES (NULL, $height, $weight, $age)";
-  // printf("Last inserted record has id %d" . mysql_insert_id());
-
-  if(mysqli_query($mysqli, $sql) === true) {
-    $last_id = $mysqli->insert_id;
-    echo "New record created successfully. Your ID is: " . $last_id;
-  }
-  else {
-      $_SESSION['message'] = "Account was not created:(";
-  }
-}
-
-$mysqli->close();
-?>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Saucy Chef</title>
-
-	<!-- [ FONT-AWESOME ICON ]
-        =========================================================================================================================-->
-	<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
-
-	<!-- [ PLUGIN STYLESHEET ]
-        =========================================================================================================================-->
-	<link rel="shortcut icon" type="image/x-icon" href="images/icon.ico">
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
-  <link rel ="stylesheet" type="text/css" href="library/vegas/vegas.min.css">
-	<!-- [ Boot STYLESHEET ]
-        =========================================================================================================================-->
-	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
-
-        <!-- [ DEFAULT STYLESHEET ]
-        =========================================================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/responsive.css">
-	<link rel="stylesheet" type="text/css" href="css/color/green.css">
-
+  <title> Login </title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body >
-<!-- [ LOADERs ]
-================================================================================================================================-->
-    <div class="preloader">
-    <div class="loader">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+<body>
+  <div class="login-page">
+    <div class="form">
+      <form class="register-form">
+        <input type="text" placeholder="name"/>
+        <input type="password" placeholder="password"/>
+        <input type="text" placeholder="email address"/>
+        <button>create</button>
+        <p class="message">Already registered? <a href="#">Sign In</a></p>
+      </form>
+      <form class="login-form">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>login</button>
+        <p class="message">Not registered? <a href="#">Create an account</a></p>
+      </form>
     </div>
-</div>
-<!-- [ /PRELOADER ]
-=============================================================================================================================-->
-<!-- [WRAPPER ]
-=============================================================================================================================-->
-<div class="wrapper">
-
- <!-- [NAV]
- ============================================================================================================================-->
-   <!-- Navigation
-    ==========================================-->
-    <section>
-    <nav  class="amd-menu navbar navbar-default navbar-fixed-top theme_background_color fadeInDown">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-            <a class="navbar-brand" href="index.html">SAUCY CHEF<span class="black"></span></a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="index.html" class="page-scroll">Home</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
-  </section>
-
-
-   <!-- [/NAV]
- ============================================================================================================================-->
-
-
-
-
-
-  <!-- [METRICS ]
-=============================================================================================================================-->
-  <section class="metrics">
-    <div class="login-page">
-      <div class="form">
-        <form class="register-form">
-          <input type="text" placeholder="name"/>
-          <input type="password" placeholder="password"/>
-          <input type="text" placeholder="email address"/>
-          <button>create</button>
-          <p class="message">Already registered? <a href="#">Sign In</a></p>
-        </form>
-        <form class="login-form">
-          <input type="text" placeholder="username"/>
-          <input type="password" placeholder="password"/>
-          <button>login</button>
-          <p class="message">Not registered? <a href="#">Create an account</a></p>
-        </form>
-      </div>
-    </div>
-  </section>
-</div>
-
-
-<!-- [ /WRAPPER ]
-=============================================================================================================================-->
-
-	<!-- [ DEFAULT SCRIPT ] -->
-	<script src="library/modernizr.custom.97074.js"></script>
-	<script src="library/jquery-1.11.3.min.js"></script>
-        <script src="library/bootstrap/js/bootstrap.js"></script>
-	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-        <script src="library/vegas/vegas.min.js"></script>
-	<!-- [ PLUGIN SCRIPT ] -->
-
-	<script src="js/plugins.js"></script>
-        <script src="js/fappear.js"></script>
-       <script src="js/jquery.countTo.js"></script>
-	<script src="js/scrollreveal.js"></script>
-       	 <!-- [ COMMON SCRIPT ] -->
-	<script src="js/common.js"></script>
-
+  </div>
 </body>
-
-
-</html>
