278d6ed8808f214b7758fb14721487a30f22be34
Adi <adarsh.ramchandran@gmail.com>
2018-11-02 01:44:57 -0500

    msg

--- profile.php
+++ profile.php
@@ -216,7 +216,7 @@ $mysqli->close();
 
   <section class="metrics">
     <form style="margin-top:80px;}" class="form" action="profile.php" method="post" enctype="multipart/form-data" autocomplete="off">
-      <div class="alert alert-error"><font color="black"></font><?= $_SESSION['results'] ?></div>
+      <div class="alert alert-error"><font color="black"><?= $_SESSION['results'] ?></font></div>
       <input type="text" placeholder="ID" name="searchId" required />
       <input type="submit" value="Search User Metrics" name="searchAccounts" class="btn btn-block" />
 
