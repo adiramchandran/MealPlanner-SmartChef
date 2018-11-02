7edd031cccf8a4e33b819f720cba946713b9ae80
Siddharth Ray <sr4@illinois.edu>
2018-11-01 20:12:38 -0500

    sdf

--- profile.php
+++ profile.php
@@ -42,7 +42,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $height = $_POST['updateHeight'];
   $weight = $_POST['updateWeight'];
   $age = $_POST['updateAge'];
-  $sql = "UPDATE User SET Height = $updateHeight, Weight = $updateWeight, Age = $updateAge WHERE ID = $updateID";
+  $sql = "UPDATE User SET Height = '$updateHeight', Weight = '$updateWeight', Age = '$updateAge' WHERE ID = '$updateID'";
   // printf("Last inserted record has id %d" . mysql_insert_id());
 
   if(mysqli_query($mysqli, $sql) === true) {
