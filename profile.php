10a49c620afd43fa9d2857c14182320e41626120
Siddharth Ray <sr4@illinois.edu>
2018-11-01 20:05:43 -0500

    sdf

--- profile.php
+++ profile.php
@@ -124,7 +124,6 @@ $mysqli->close();
 
 
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
