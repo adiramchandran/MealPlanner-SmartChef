53c64dbd1f11264a670cdca034ae51746cbcecf4
Siddharth Ray <sr4@illinois.edu>
2018-11-01 20:03:00 -0500

    sdf

--- profile.php
+++ profile.php
@@ -35,6 +35,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 
+
 // UPDATE 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $id = $_POST['updateID'];
@@ -124,7 +125,6 @@ $mysqli->close();
 
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
