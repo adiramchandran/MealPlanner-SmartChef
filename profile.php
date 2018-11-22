669e0feb1d53b8bd0dbc4a4c9f2776f245a54079
Adi <adarsh.ramchandran@gmail.com>
2018-11-22 14:19:57 -0800

    testing

--- profile.php
+++ profile.php
@@ -25,6 +25,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $weight_per_wk = $_POST['weight_per_wk'];
   $lifestyle = $_POST['lifestyle'];
   $gender = $_POST['gender'];
+  $bmr = 0.0;
+  $cal = 0.0;
   /*
   $bmr = 0.0;
 
