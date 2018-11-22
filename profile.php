345ff5fb109048e9507dd1b9cc047535db7c35b7
Adi <adarsh.ramchandran@gmail.com>
2018-11-22 13:42:48 -0800

    testing

--- profile.php
+++ profile.php
@@ -25,6 +25,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $weight_per_wk = $_POST['weight_per_wk'];
   $lifestyle = $_POST['lifestyle'];
   $gender = $_POST['gender'];
+  $bmr = 0.0
+  $cal = $bmr + ( ($weight_per_wk * 3500.) / 7. )
   /*
   $bmr = 0.0
   if ($gender == 'f'){ // calc female BMR expression
