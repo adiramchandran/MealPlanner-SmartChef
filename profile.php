1eb3484da94c8f250f39e5100d2c0a2398e86b86
Adi <adarsh.ramchandran@gmail.com>
2018-11-22 14:10:44 -0800

    testing

--- profile.php
+++ profile.php
@@ -27,13 +27,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $gender = $_POST['gender'];
 
   $bmr = 0.0;
-  /*
+
   if ($gender == "f"){ // calc female BMR expression
       $bmr += 655 + (4.35 * $weight) + (4.7 * $height) - (4.7 * $age);
   }
   else{                 // calc male BMR expression
       $bmr += 66 + (6.23 * $weight) + (12.7 * $height) - (6.8 * $age);
   }
+  /*
   // account for lifestyle (scale of 1 -> 5; sedentary to extremely active)
   switch ($lifestyle){
       case 1:
@@ -57,7 +58,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   // use BMR to calc target calories per day
   $cal = $bmr + ( ($weight_per_wk * 3500) / 7 );
   */
-  
+
   $sql = "INSERT INTO User (ID, Height, Weight, Age, Weight_per_Wk, Lifestyle, Gender, BMR, Cal_per_day) " . " VALUES (NULL, $height, $weight, $age, $weight_per_wk, $lifestyle, $gender, $bmr, $cal)";
   // printf("Last inserted record has id %d" . mysql_insert_id());
 
