bc9fdf14b86cc524bba3d792e404d473882985ff
Jessica Gomes <jessicagomes@wirelessprv-10-192-185-72.near.illinois.edu>
2018-12-01 16:48:58 -0600

    doing login stuff

--- profile.php
+++ profile.php
@@ -37,8 +37,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
   $weight_per_wk = $_POST['weight_per_wk'];
   $lifestyle = $_POST['lifestyle'];
   $gender = $_POST['gender'];
-  echo "gender: " . $_POST['gender'];
-
+  
   $bmr = 0.0;
   if ($gender == "f"){ // calc female BMR expression
       $bmr += 655 + (4.35 * $weight) + (4.7 * $height) - (4.7 * $age);
