b09bb4a501e6b589cc3c955648f78559ce59df57
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-12-02 13:52:26 -0600

    doing login stuff

--- dailyplan.php
+++ dailyplan.php
@@ -1,27 +1,134 @@
-<!doctype html>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<!--
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+AUTHOR : Vijayan PP
+PROJECT :A-MD
+VERSION : 1.1
+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-->
 <html>
 <head>
-<meta charset="utf-8">
-<title>Daily Plan</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Daily Plan</title>
+
+	<!-- [ FONT-AWESOME ICON ]
+        =========================================================================================================================-->
+	<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	<!-- [ PLUGIN STYLESHEET ]
+        =========================================================================================================================-->
+	<link rel="shortcut icon" type="image/x-icon" href="images/icon.ico">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+        <link rel ="stylesheet" type="text/css" href="library/vegas/vegas.min.css">
+	<!-- [ Boot STYLESHEET ]
+        =========================================================================================================================-->
+	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+
+        <!-- [ DEFAULT STYLESHEET ]
+        =========================================================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="css/color/green.css">
 
 </head>
+<body >
+<!-- [ LOADERs ]
+================================================================================================================================-->
+    <div class="preloader">
+    <div class="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
+<!-- [ /PRELOADER ]
+=============================================================================================================================-->
+<!-- [WRAPPER ]
+=============================================================================================================================-->
+<div class="wrapper">
 
-<body>
-<?php
-$mysqli = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-if ($mysqli) {
-  echo "Connection established! <br>";
-}
-else {
-  echo "Connection failed!";
-}
+ <!-- [NAV]
+ ============================================================================================================================-->
+   <!-- Navigation
+    ==========================================-->
+    <nav  class="amd-menu navbar navbar-default navbar-fixed-top theme_background_color fadeInDown">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+            <a class="navbar-brand" href="index.html">SAUCY CHEF<span class="black"></span></a>
+        </div>
 
-$sql = "SELECT * FROM RecipeList";
-$results = mysqli_query($mysqli, $sql);
-while ($row = mysqli_fetch_assoc($results)) {
-  echo $row["title"]." ".$row["url"]." ".$row["calories"]." ".$row["fat"]." ".$row["protein"]." ".$row["carbs"]." ".$row["Breakfast"];
-  echo "<br>";
-}
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#home" class="page-scroll">Home</a></li>
+            <li><a href="profile.php" class="page-scroll">My Profile</a></li>
+            <li><a href="dailyplan.php" class="page-scroll">Weekly Plan</a></li>
+            <li><a href="#home" class="page-scroll">Cookbook</a></li>
+            <li><a>Welcome, <?php echo $_SESSION['username'];?>!</a>
+            <li><a href="logout.php" class="page-scroll">Logout</a></li>
+            <li><a href="#contact" class="page-scroll" id="contacts"><i class="fa fa-send"></i></a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
 
 
- ?>
+   <!-- [/NAV]
+ ============================================================================================================================-->
+
+<div>
+  <?php
+  $mysqli = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
+  if ($mysqli) {
+    echo "Connection established! <br>";
+  }
+  else {
+    echo "Connection failed!";
+  }
+
+  $sql = "SELECT * FROM RecipeList";
+  $results = mysqli_query($mysqli, $sql);
+  while ($row = mysqli_fetch_assoc($results)) {
+    echo $row["title"]." ".$row["url"]." ".$row["calories"]." ".$row["fat"]." ".$row["protein"]." ".$row["carbs"]." ".$row["Breakfast"];
+    echo "<br>";
+  }
+
+
+   ?>
+</div>
+
+	<!-- [ DEFAULT SCRIPT ] -->
+	<script src="library/modernizr.custom.97074.js"></script>
+	<script src="library/jquery-1.11.3.min.js"></script>
+        <script src="library/bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script src="library/vegas/vegas.min.js"></script>
+	<!-- [ PLUGIN SCRIPT ] -->
+
+	<script src="js/plugins.js"></script>
+        <script src="js/fappear.js"></script>
+       <script src="js/jquery.countTo.js"></script>
+	<script src="js/scrollreveal.js"></script>
+       	 <!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+
+</body>
+
+
+</html>
