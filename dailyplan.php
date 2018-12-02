813d16511bd1c5827b32f4efb59816090fc36a49
Jessica Gomes <jessicagomes@host-243-95.iledenw.edwardsville.il.us.clients.pavlovmedia.net>
2018-12-02 17:46:35 -0600

    doing login stuff

--- dailyplan.php
+++ dailyplan.php
@@ -163,7 +163,7 @@ VERSION : 1.1
     $results = mysqli_query($mysqli, $sql);
     $i = 1;
     $two = 2;
-    while ($row = mysqli_fetch_assoc($results)) {
+    while ($row = mysqli_fetch_assoc($results) && $row["Breakfast"] == 1) {
         if ($row["calories"] <= $_SESSION['numCalories']*0.2) {
           echo "Breakfast: " . $_SESSION['numCalories']*0.2;
           echo "<h1>" . $row["title"] . "</h1>";
