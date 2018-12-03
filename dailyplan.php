b804b33f8b34558eca1dc2b78eb0901c77365df2
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2018-12-02 22:38:58 -0600

    doing favorites stuff

--- dailyplan.php
+++ dailyplan.php
@@ -10,7 +10,7 @@ function removeFunction() {
 }
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
   $mysqli = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-  // $sql = "INSERT INTO UserFavorites (ID, RecipeName) " . " VALUES ('$_SESSION[\'user_id\']', '$_SESSION[\'breakfast\']')";
+  $sql = "INSERT INTO UserFavorites (ID, RecipeName) " . " VALUES ('$_SESSION[\'user_id\']', '$_SESSION[\'breakfast\']')";
   // $results = mysqli_query($mysqli, $sql);
   // if ($results) {
   //   echo "succeeded";
