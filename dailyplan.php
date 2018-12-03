d9804dfb0d05b985c9cf15ec8203ae8dfa3a3476
Jessica Gomes <jessicagomes@host-243-95.iledenw.edwardsville.il.us.clients.pavlovmedia.net>
2018-12-02 18:10:35 -0600

    doing login stuff

--- dailyplan.php
+++ dailyplan.php
@@ -3,6 +3,30 @@ if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
     session_cache_limiter("public");
 }
 session_start();
+function findBreakfast() {
+  $mysqli = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
+  $sql = "SELECT * FROM RecipeList order by rand()";
+  $results = mysqli_query($mysqli, $sql);
+  $i = 1;
+  $two = 2;
+  while ($row = mysqli_fetch_assoc($results) and $row["Breakfast"] == 'B') {
+      if ($row["calories"] <= $_SESSION['numCalories']*0.2) {
+        echo "Breakfast: " . $_SESSION['numCalories']*0.2;
+        echo "<h1>" . $row["title"] . "</h1>";
+        echo '<br>';
+        echo "Calories: " . $row["calories"];
+        echo '<br>';
+        echo "Fat: " . $row["fat"] . " grams";
+        echo '<br>';
+        echo "Protein: " . $row["protein"] . " grams";
+        echo '<br>';
+        echo "Carbohydrates: " . $row["carbs"] . " grams";
+        echo '<br>';
+        echo '<button class=button onclick="window.location.href=\'' . $row["url"] . '\'">View Recipe Now</button>';
+        break;
+      }
+  }
+}
 ?>
 
 <!DOCTYPE html>
@@ -156,33 +180,8 @@ VERSION : 1.1
  ============================================================================================================================-->
 <div class="row">
   <div class="column1">
-    <?php
-    function findBreakfast() {
-      $mysqli = mysqli_connect("127.0.0.1", "teamsaauuwwce_teamsauce", "Teamsauce", "teamsaauuwwce_tempdatabase");
-      $sql = "SELECT * FROM RecipeList order by rand()";
-      $results = mysqli_query($mysqli, $sql);
-      $i = 1;
-      $two = 2;
-      while ($row = mysqli_fetch_assoc($results) and $row["Breakfast"] == 'B') {
-          if ($row["calories"] <= $_SESSION['numCalories']*0.2) {
-            echo "Breakfast: " . $_SESSION['numCalories']*0.2;
-            echo "<h1>" . $row["title"] . "</h1>";
-            echo '<br>';
-            echo "Calories: " . $row["calories"];
-            echo '<br>';
-            echo "Fat: " . $row["fat"] . " grams";
-            echo '<br>';
-            echo "Protein: " . $row["protein"] . " grams";
-            echo '<br>';
-            echo "Carbohydrates: " . $row["carbs"] . " grams";
-            echo '<br>';
-            echo '<button class=button onclick="window.location.href=\'' . $row["url"] . '\'">View Recipe Now</button>';
-            break;
-          }
-      }
-    }
-
-    findBreakfast();
+  <?php
+      findBreakfast();
   ?>
   <input type="button" class=button value="Shuffle" onclick="findBreakfast()"/>
   </div>
