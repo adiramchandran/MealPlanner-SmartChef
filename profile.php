26eabe66a60bbfd1dfcd02a9c2d222eda46b7deb
Siddharth Ray <sr4@illinois.edu>
2018-12-03 18:42:59 -0600

    prof

--- profile.php
+++ profile.php
@@ -36,13 +36,7 @@ if (!empty($_POST['showMetrics'])) {
   $result = mysqli_query($mysqli, $sql);
   if(mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-      $_SESSION['search_out'] = "Username: " . $row["Username"] . " \n ". "Height: " . $row["Height"];
-        echo "Username: " . $row["Username"];
-        echo "Height: " . $row["Height"];
-        echo "Weight: " . $row["Weight"];
-        echo "Age: " . $row["Age"];
-        echo "Weight Change Per Week (lbs): " . $row["Weight_per_wk"];
-        echo "Lifestyle Rating " . $row["Lifestyle"];
+      $_SESSION['search_out'] = "Username: " . $row["Username"] . " \n ". "Height: " . $row["Height"] . "Weight: " . $row["Weight"] . "Age: " . $row["Age"] . "Weight Change Per Week (lbs): " . $row["Weight_per_wk"] . "Lifestyle Rating " . $row["Lifestyle"];
         if ($row["Gender"] == 1){
           echo "Gender: Male";
         }
