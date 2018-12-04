4137cc4784edd990f9f27b3575fe40637180edf2
Jessica Gomes <jessicagomes@wirelessprv-10-192-185-72.near.illinois.edu>
2018-12-03 21:10:30 -0600

    doing favorites stuff

--- favorites.php
+++ favorites.php
@@ -10,7 +10,7 @@ function listFavorites() {
   $results = mysqli_query($mysqli, $sql);
   while ($row = mysqli_fetch_assoc($results)) {
         echo "<div class='box-one'>";
-        echo "<h1>" . $row["title"] . "</h1>";
+        echo "<h2>" . $row["title"] . "</h2>";
         echo '<br>';
         echo "Calories: " . $row["calories"];
         echo '<br>';
